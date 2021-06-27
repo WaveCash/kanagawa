0788c534dec446dfc8d461f6adc3db9bbef1fd02
Lucien Song <bitcorn@gmx.com>
2021-06-28 02:02:52 +0800

    New UI
    New UI

---
+++ lang/languages.js
@@ -0,0 +1,16 @@
+export default [
+  {
+    code: 'en',
+    iso: 'en-US',
+    name: 'English',
+    file: 'en-US.js',
+    dir: 'ltr'
+  },
+  {
+    code: 'zh',
+    iso: 'zh-ZH',
+    name: 'Chinese',
+    file: 'zh-ZH.js',
+    dir: 'ltr'
+  }
+]
---
+++ text/brand.js
@@ -0,0 +1,16 @@
+const brand = {
+  crypto: {
+    name: 'Wave Cash',
+    desc: 'Veluxi Cryptocurrency - Vue Single Landing Page Template',
+    prefix: 'wave.cash',
+    footerText: 'The Great Wave 2021',
+    logoText: 'The Great Wave',
+    projectName: 'The Great Wave',
+    url: 'https://wave.cash/',
+    img: '/static/images/coin@256x256.png',
+    notifMsg:
+      'Donec sit amet nulla sed arcu pulvinar ultricies commodo id ligula.'
+  }
+}
+
+export default brand
