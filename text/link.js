05d55327140dfe5097e75ab636bb3da3c69c03e6
Lucien Song <LucienSong@gmx.com>
2021-12-21 01:30:37 +0800

    Update NFT link
    Update NFT link

--- text/link.js
+++ text/link.js
@@ -7,7 +7,7 @@ const link = {
     airdrop: '/airdrop',
     ocean: '/ocean',
     exchange: 'https://exchange.pancakeswap.finance/#/swap?outputCurrency=0x46cbe14f4ab4c665e084a0a868c49098f0ce7704',
-    nft: 'https://wavecash.github.io/Snake-Chain-NFT-Game/'
+    nft: 'https://fervent-carson-1139e0.netlify.app/'
   }
 }
 
