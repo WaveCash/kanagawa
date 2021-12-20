0645dc625616d6bc06111457d53c641b29dee162
Lucien Song <LucienSong@gmx.com>
2021-12-20 14:12:15 +0800

    Update link

--- text/link.js
+++ text/link.js
@@ -7,7 +7,7 @@ const link = {
     airdrop: '/airdrop',
     ocean: '/ocean',
     exchange: 'https://exchange.pancakeswap.finance/#/swap?outputCurrency=0x46cbe14f4ab4c665e084a0a868c49098f0ce7704',
-    nft: 'https://snakechain.net'
+    nft: 'https://wavecash.github.io/Snake-Chain-NFT-Game/'
   }
 }
 
