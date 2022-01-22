45a3d5c7cc54cf7e220065fcff365e50ae3bb52f
Lucien Song <LucienSong@gmx.com>
2022-01-22 16:38:46 +0800

    Update NFT link
    Update NFT link

--- text/link.js
+++ text/link.js
@@ -7,7 +7,7 @@ const link = {
     airdrop: '/airdrop',
     ocean: '/ocean',
     exchange: 'https://exchange.pancakeswap.finance/#/swap?outputCurrency=0x46cbe14f4ab4c665e084a0a868c49098f0ce7704',
-    nft: 'https://nft-pre-sale.netlify.app/'
+    nft: 'https://nft.wave.cash/'
   }
 }
 
